c4607ba69d58d47597b680837cf111fcb484bff7
Esteves31 <matheus.resteves@gmail.com>
2023-01-12 13:14:32 -0300

    feat: add Pet class

--- disclose/models.py
+++ disclose/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Breed(models.Model):
     breed = models.CharField(max_length=50)
@@ -11,3 +12,20 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag
+
+class Pet(models.Model):
+    choice_status = (('P', 'Para adoção'),
+                     ('A', 'Adotado'))
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    photo = models.ImageField(upload_to="pets_photos")
+    pet_name = models.CharField(max_length=100)
+    description = models.TextField()
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    number_phone = models.CharField(max_length=14)
+    tags = models.ManyToManyField(Tag)
+    breed = models.ForeignKey(Breed, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=1, choices=choice_status)
+
+    def __str__(self):
+        return self.pet_name
