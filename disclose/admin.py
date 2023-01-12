4a22a8190ac4a900399a68590667d0c47d285f21
Esteves31 <matheus.resteves@gmail.com>
2023-01-12 13:17:01 -0300

    feat: register Pet table

--- disclose/admin.py
+++ disclose/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Breed, Tag
+from .models import Breed, Tag, Pet
 
 admin.site.register(Breed)
 admin.site.register(Tag)
+admin.site.register(Pet)
