8f7d9ec2b286e80a6cc0420b409684578370cfce
Esteves31 <matheus.resteves@gmail.com>
2023-01-13 23:33:53 -0300

    feat: add remove_pet url

--- disclose/urls.py
+++ disclose/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('novo_pet/', views.new_pet, name="new_pet"),
     path('seus_pets/', views.your_pets, name="your_pets"),
+    path('remover_pet/<int:id>', views.remove_pet, name="remove_pet"),
 ]
