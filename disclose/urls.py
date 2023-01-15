9aa167f87c5311e4b725e34a1e6dcf2e34015c54
Esteves31 <matheus.resteves@gmail.com>
2023-01-15 10:44:33 -0300

    feat: add see_pet url

--- disclose/urls.py
+++ disclose/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('novo_pet/', views.new_pet, name="new_pet"),
     path('seus_pets/', views.your_pets, name="your_pets"),
     path('remover_pet/<int:id>', views.remove_pet, name="remove_pet"),
+    path('ver_pet/<int:id>', views.see_pet, name="see_pet"),
 ]
