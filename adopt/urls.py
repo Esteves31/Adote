71427435b6f5507c3474973ad667723c1a6653ab
Esteves31 <matheus.resteves@gmail.com>
2023-01-15 14:52:11 -0300

    feat: add processes_adopt_application url

--- adopt/urls.py
+++ adopt/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.list_pets, name="list_pets"),
     path('pedido_adocao/<int:id_pet>', views.adoption_application, name="adoption_application"),
+    path('processa_pedido_adocao/<int:id_request>', views.processes_adopt_application, name="processes_adopt_application"),
 ]
